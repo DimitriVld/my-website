1e05336587fb0d8ca5d75efaa9c09e3a99e8f675
Dimitri Vildina <Dimitri.vildina@gmail.com>
2022-10-30 21:13:12 +0100

    Add animation header

--- src/views/component/Menu.js
+++ src/views/component/Menu.js
@@ -2,17 +2,17 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Menu = () => {
-    return(
-        <div className={'c-menu'} >
-            <nav className='c-menu-nav'>
-                    <Link to='/' className={'c-menu-link'}>Homepage</Link>
-                    <Link to='/works' className={'c-menu-link'}>Our works</Link>
-                    <Link to='/services' className={'c-menu-link'}>Our Services</Link>
-                    <Link to='/about-us' className={'c-menu-link'}>About Us</Link>
-                    <Link to='/contact' className={'c-menu-link'}>Contact us</Link>
-                </nav>
-        </div>
-    )
+  return(
+    <div className={'c-menu'} >
+      <nav className='c-menu-nav'>
+        <Link to='/' className={'c-menu-link'}>Homepage</Link>
+        <Link to='/works' className={'c-menu-link'}>Our works</Link>
+        <Link to='/services' className={'c-menu-link'}>Our Services</Link>
+        <Link to='/about-us' className={'c-menu-link'}>About Us</Link>
+        <Link to='/contact' className={'c-menu-link'}>Contact us</Link>
+      </nav>
+    </div>
+  )
 }
 
 export default Menu;
--- src/views/layout/DefaultLayout.js
+++ src/views/layout/DefaultLayout.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from '../component/Header';
+import Menu from '../component/Menu';
 
 const DefaultLayout = (props) => {
     return (
         <div className={"default-layout"}>
             <Header />
+            <Menu />
             <main className={"main-content"}>{props.page}</main>
         </div>
     );
