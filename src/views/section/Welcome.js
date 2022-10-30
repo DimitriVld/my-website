18a8aa6508c802770e75cdaebcca8dcf85f1e36b
Dimitri Vildina <Dimitri.vildina@gmail.com>
2022-10-30 03:44:20 +0100

    Section welcome in progress

--- src/views/section/Welcome.js
+++ src/views/section/Welcome.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import Logo from '../icon/logo.svggit';
+import LogoIcon from '../icon/Logo';
 
 const WelcomeSection = (props) => {
     return (
-        <div className={`s-welcome height-100vh home ${props.services ? 'services' : ''} ${props.about ? 'about' : ''}`}>
+        <div className={`s-welcome height-100-vh home ${props.services ? 'services' : ''} ${props.about ? 'about' : ''}`}>
             <div className="container-dft">
-                <img className="s-welcome-logo" src="{Logo}" alt="logo" />
+                <LogoIcon class='s-welcome-logo' />
                 <h1 className='s-welcome-title ui-h1 is-big'>{props.title}</h1>
                 <p className='s-welcome-subtitle ui-h1 is-medium'>{props.subTitle}</p>
                 <p className="s-welcome-scroll">Scroll</p>
