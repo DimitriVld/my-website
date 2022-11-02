2d203ae919873024b66797f7267d5eddd695116c
Dimitri Vildina <Dimitri.vildina@gmail.com>
2022-11-02 23:07:07 +0100

    Start services pages

--- src/views/page/Services.js
+++ src/views/page/Services.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import WelcomeSection from '../section/Welcome';
 
 const Services = () => { 
 
   return (
     <div>
-      Services
+      <WelcomeSection
+        title='Dimitri Vildina'
+        subTitle={["At AD. Agency, We are committed to defining for you, maintainable digital solutions, scalable, efficient and secure to better meet your needs.",  <br></br>,  <br></br>, "Thanks to the variety and versatility of our talented employees, we can intervene and follow from A to Z on all your projects.Let's do it!"]}
+        services='true'
+      />
     </div>
   );
 };
