d932f37c4acf949878a02c85581606cad1405a38
Dimitri Vildina <Dimitri.vildina@gmail.com>
2022-11-04 23:51:24 +0100

    Contact page

--- src/views/page/Contact.js
+++ src/views/page/Contact.js
@@ -1,10 +1,34 @@
 import React from 'react';
+import LogoIcon from '../icon/Logo';
 
 const Contact = () => { 
 
   return (
-    <div>
-      Contact
+    <div className='p-contact'>
+      <div className='container'>
+        <LogoIcon class='p-contact-logo' />
+        <h1 className='p-contact-title ui-h1'>Send me a message !</h1>
+        <p className='p-contact-text'>Got a question or proposal, or just want <br /> to say hello ? Go ahead.</p>
+        <form>
+          <div className='p-contact-inputGroup'>
+            <fieldset className=''>
+              <label className='display-b'>Label</label>
+              <input className='p-contact-input' type='text' placeholder='input' />
+            </fieldset>
+
+            <fieldset className=''>
+              <label className='display-b'>Label</label>
+              <input className='p-contact-input' type='text' placeholder='input' />
+            </fieldset>
+          </div>
+          <fieldset>
+            <label className='display-b'>Label</label>
+            <textarea className='p-contact-textarea'></textarea>
+          </fieldset>
+
+          <button className='p-contact-btn' type='submit'>Send</button>
+        </form>
+      </div>
     </div>
   );
 };
