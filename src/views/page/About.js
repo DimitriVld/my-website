5011ea8f515cab226d9a1b018131aa3bfa0751dc
Dimitri Vildina <Dimitri.vildina@gmail.com>
2022-11-04 21:35:57 +0100

    Culture section

--- src/views/page/About.js
+++ src/views/page/About.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import WelcomeSection from '../section/Welcome';
+import CultureSection from '../section/Culture';
 
 const About = () => { 
 
@@ -10,6 +11,7 @@ const About = () => {
       subtitle='know everything about us'
       isAbout='true'
       />
+      <CultureSection />
     </div>
   );
 };
---
+++ src/views/section/Culture.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const Culture = () => {
+  return (
+    <div className={'s-culture'}>
+      <div className='container'>
+        <h3 className='ui-h1'>Our culture,<br/> values and <br/>belief</h3>
+        <div className='cultures'>
+          <div className='culture'>
+            <h4>Title</h4>
+            <p>Content</p>
+          </div>
+          <div className='culture'>
+            <h4>Title</h4>
+            <p>Content</p>
+          </div>
+          <div className='culture'>
+            <h4>Title</h4>
+            <p>Content</p>
+          </div>
+        </div>
+      </div>
+    </div>
+);
+}
+
+export default Culture;
