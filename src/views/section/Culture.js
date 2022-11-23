1c48319da38939700a353066a72fac636bd762f2
Dimitri Vildina <Dimitri.vildina@gmail.com>
2022-11-23 16:02:53 +0100

    Section culture

--- src/views/section/Culture.js
+++ src/views/section/Culture.js
@@ -7,16 +7,25 @@ const Culture = () => {
         <h3 className='ui-h1'>Our culture,<br/> values and <br/>belief</h3>
         <div className='cultures'>
           <div className='culture'>
-            <h4>Title</h4>
-            <p>Content</p>
+            <h4>#Performance</h4>
+            <p>
+            Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. 
+            Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, 
+            </p>
           </div>
           <div className='culture'>
-            <h4>Title</h4>
-            <p>Content</p>
+            <h4>#Maintenance</h4>
+            <p>
+            Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. 
+            Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, 
+            </p>
           </div>
           <div className='culture'>
-            <h4>Title</h4>
-            <p>Content</p>
+            <h4>#Security</h4>
+            <p className='ui-p'>
+            Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. 
+            Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, 
+            </p>
           </div>
         </div>
       </div>
